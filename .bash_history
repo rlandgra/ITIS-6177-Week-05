bb53fae8c890b0524b0f6b1a11476b337b36d6cb
Ross Landgraf <rlandgra@uncc.edu>
2022-09-27 19:03:06 +0000

    First Commit

---
+++ .bash_history
@@ -0,0 +1,90 @@
+nano _prepare_server.sh
+vi _prepare_server.sh
+nano _prepare_server.sh
+sudo apt install nano
+sudo yum install nano
+nano _prepare_server.sh
+-ls
+-ls a
+-ls lah
+ls -la
+ls -lah
+chmod 744 _prepare_server.sh 
+ls -lah
+./_prepare_server.sh 
+mysql_secure_installation
+curl https://www.w3resource.com/sql/sample-database-of-sql-in-mysql-format.txt --output db.sql
+ls
+mysql -u root -p
+mysql -u root -p sample < db.sql
+mkdir server
+cd server
+npm init -y && npm i mariadb express
+ls
+nano server.js
+node server.js
+nano server.js
+node server.js
+nano server.js
+ls
+node server.js
+ls
+cd server
+node server.js
+npm install cors
+node server.js
+npm i mariadb
+node server.js
+npm install nodemon
+nodemon server.js
+mysql
+mysql -p
+nano server.js
+mysql -p
+cd server
+nodemon server.js
+npm ls
+npm install -g nodemon
+nodemon server.js
+npm i swagger-jsdoc
+npm i swagger-ui-express
+nodemon app.js
+nodemon server.js
+node server.js
+nodemon server.js
+node server.js
+cd server
+node server.js
+cd server
+nodemon server.js
+npm i express
+nodemon server.js
+npm i swagger-jsdoc
+npm i swagger-ui-express
+nodemon server.js
+node server.js
+grep
+lsof -i tcp:3000
+ps -e|grep node
+ps -e|grep nodemon
+lsof -i tcp:3000
+ps -i tcp:3000
+ps tcp:3000
+fuser 3000/tcp
+nodemon server.js
+cd server
+nodemon server.js
+npm i swagger-jsdoc
+npm i swagger-ui-express
+nodemon server.js
+node server.js
+sudo npm install -g forever
+forever server.js
+forever start server.js
+forever -m5 server.js
+forever restart 0
+cd server
+node server.js
+cd server
+node server.js
+forever start server.js
---
+++ .vscode-server/data/User/History/2e0c0582/UaSS.js
@@ -0,0 +1,136 @@
+const express = require('express');
+
+// const swaggerJsdoc = require('swagger-jsdoc');
+// const swaggerUi = require('swagger-ui-express');
+
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'sample',
+	port: 3306,
+	connectionLimit: 5
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+res.send('Welcome to the default site, try urls with /customers or /agents or /foods or /agents/(Agent Code)');
+})
+
+app.get('/test', (req, res) => {
+	res.send('This is a test');
+	})
+
+//---------------------------------------------------------------------------GET REQUESTS:---------------------------------------------------------------------------
+
+app.get('/customers', (req, res) => {
+pool.getConnection()
+    .then(conn => {
+    
+      conn.query("SELECT * FROM customer")
+        .then((rows) => {
+          console.log(rows);
+		  res.setHeader('Content-Type', 'application/json');
+		  conn.end();
+		  return res.json(rows);
+          
+        })
+        .catch(err => {
+          console.log(err); 
+          conn.end();
+        })
+        
+    }).catch(err => {
+    });
+})
+
+app.get('/agents', (req, res) => {
+	pool.getConnection()
+		.then(conn => {
+		
+		  conn.query("SELECT * FROM agents")
+			.then((rows) => {
+			  console.log(rows);
+			  res.setHeader('Content-Type', 'application/json');
+			  conn.end();
+			  return res.json(rows);
+			  
+			})
+			.catch(err => {
+			  console.log(err); 
+			  conn.end();
+			})
+			
+		}).catch(err => {
+		});
+	})
+
+	app.get('/foods', (req, res) => {
+		pool.getConnection()
+			.then(conn => {
+			
+			  conn.query("SELECT * FROM foods")
+				.then((rows) => {
+				  console.log(rows);
+				  res.setHeader('Content-Type', 'application/json');
+				  conn.end();
+				  return res.json(rows);
+				  
+				})
+				.catch(err => {
+				  console.log(err); 
+				  conn.end();
+				})
+				
+			}).catch(err => {
+			});
+		})
+
+
+		app.get('/agents/:agentCode', (req, res) => {
+			pool.getConnection()
+				.then(conn => {
+				
+					conn.query("SELECT * FROM agents WHERE AGENT_CODE=?", [req.params.agentCode])
+					.then((rows) => {
+					  console.log(rows);
+					  res.setHeader('Content-Type', 'application/json');
+					  conn.end();
+					  return res.json(rows);
+					  
+					})
+					.catch(err => {
+					  console.log(err); 
+					  conn.end();
+					})
+					
+				}).catch(err => {
+				});
+			})
+
+
+//---------------------------------------------------------------------------POST REQUESTS:---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------PATCH REQUESTS:---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------PUT REQUESTS:---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------DELETE REQUESTS:---------------------------------------------------------------------------
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
